8b38a55dc2794f3fb15a6736d166e949309c21be
Amy van der Meijden <A.vanderMeijden@student.tudelft.nl>
2026-06-10 17:11:58 +0200

    multiple block messages

--- Lab3/message_payloads.py
+++ Lab3/message_payloads.py
@@ -46,7 +46,20 @@ class BlockResponse(VariablePayload):
 
 # ─── Blockchain community messages ────────────────────────
 
+class GetMultipleBlocks(VariablePayload):
+    msg_id = 7
+    format_list = ["q"]
+    names = ["start_height"]
+
+class MultipleBlocksResponse(VariablePayload):
+    msg_id = 8
+    format_list = ["q", "q", "varlenH"]
+    names = ["start_height", "num_blocks", "blocks_data"]
+
 # class BroadcastBlock(VariablePayload):
 #     msg_id = 7
 #     format_list = ["q", "varlenH", "varlenH", "varlenH", "q", "q", "q", "varlenH"]
 #     names = ["height", "prev_hash", "txs_hash", "block_hash", "timestamp", "difficulty", "nonce", "tx_hashes"]
+
+
+
